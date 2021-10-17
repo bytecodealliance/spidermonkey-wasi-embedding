f837809915300cb94e122cba6e97d0d16dfd256d
Till Schneidereit <till@tillschneidereit.net>
2021-10-17 13:12:10 +0200

    Update to SpiderMonkey 94beta (#3)
    This PR updates to the current beta of SpiderMonkey 94. Previously we were using an arbitrary commit from the Nightly 92 cycle. Switching to beta means that we get more stability, but also get some important improvements that landed in that cycle. Over time, we should switch to stable releases, though.
    
    This PR also improves some aspects of the build script:
    - It can now be run from other directories, while still resolving its dependencies correctly
    - It continues working correctly when there already is a checkout of the gecko-dev repository, and updates that to the expected revision
    - It spends significantly less time on cloning the gecko-dev repository by not unconditionally fetching all tags and branches

--- build-engine.sh
+++ build-engine.sh
@@ -1,30 +1,47 @@
 #!/usr/bin/env bash
 set -ex
 
+working_dir="$(pwd)"
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# Ensure apt-get is current, because otherwise bootstrapping might fail
+sudo apt-get update -y
+
+# Ensure the Rust version matches that used by Gecko, and can compile to WASI
+rustup update 1.55.0
+rustup default 1.55.0
+rustup target add wasm32-wasi
+
 if [[ ! -a gecko-dev ]]
 then
-  # Ensure apt-get is current, because otherwise bootstrapping might fail
-  sudo apt-get update -y
-
-  # Ensure the Rust version matches that used by Gecko, and can compile to WASI
-  rustup update 1.54.0
-  rustup default 1.54.0
-  rustup target add wasm32-wasi
 
   # Clone Gecko repository at the required revision
   mkdir gecko-dev
   cd gecko-dev
 
   git init
-  git remote add origin $(cat ../gecko-repository)
-  git fetch --depth 1 origin $(cat ../gecko-revision)
-  git checkout FETCH_HEAD
-
-  # Use Gecko's build system bootstrapping to ensure all dependencies are installed
-  ./mach bootstrap --application-choice=js
-
+  git remote add --no-tags -t wasi-embedding origin $(cat $script_dir/gecko-repository)
   cd ..
 fi
+
+cd gecko-dev
+git fetch --depth 1 origin $(cat $script_dir/gecko-revision)
+git checkout FETCH_HEAD
+
+# Use Gecko's build system bootstrapping to ensure all dependencies are installed
+./mach bootstrap --application-choice=js
+
+# ... except, that doesn't install the wasi-sysroot, which we need, so we do that manually.
+cd ~/.mozbuild
+_virtualenvs/mach/bin/python \
+  ${working_dir}/gecko-dev/mach \
+  artifact \
+  toolchain \
+  --bootstrap \
+  --from-build \
+  sysroot-wasm32-wasi
+
+cd ${working_dir}
 
 flags="--optimize --no-debug --build-only"
 rust_lib_dir="release"
@@ -37,12 +54,13 @@ fi
 echo $flags $rust_lib_dir
 
 # Build SpiderMonkey for WASI
-MOZ_FETCHES_DIR=~/.mozbuild gecko-dev/js/src/devtools/automation/autospider.py --objdir=obj $flags wasi
+MOZ_FETCHES_DIR=~/.mozbuild CC=~/.mozbuild/clang/bin/clang gecko-dev/js/src/devtools/automation/autospider.py --objdir=obj $flags wasi
 
 # Copy header, object, and static lib files
+rm -rf lib include
 mkdir lib
 
 cd obj
 cp -Lr dist/include ..
-cp $(cat ../object-files.list) ../lib
+cp $(cat $script_dir/object-files.list) ../lib
 cp js/src/build/libjs_static.a wasm32-wasi/${rust_lib_dir}/libjsrust.a ../lib
