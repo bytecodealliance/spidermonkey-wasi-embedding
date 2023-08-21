193e773daf2c445695d36b150e4493fae51dd603
Chris Fallin <chris@cfallin.org>
2023-08-21 13:37:55 -0700

    Add Portable Baseline Interpreter option to build configurations.

--- build-engine.sh
+++ build-engine.sh
@@ -25,6 +25,7 @@ ac_add_options --disable-clang-plugin
 ac_add_options --enable-jitspew
 ac_add_options --enable-optimize
 ac_add_options --enable-js-streams
+ac_add_options --enable-portable-baseline-interp
 ac_add_options --prefix=${working_dir}/${objdir}/dist
 mk_add_options MOZ_OBJDIR=${working_dir}/${objdir}
 mk_add_options AUTOCLOBBER=1
